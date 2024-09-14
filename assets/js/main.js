e3ec4a8147f769c8b956cef314f4c5b8b8bccdeb
Junior Vasquez <junior.vasquez07@gmail.com>
2024-09-13 21:06:42 -0500

    Skills, Works and Contact sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,22 @@ menu.addEventListener("click",()=>{
         navbar.classList.add("open")
     }
 });
+
+/*==================== SKILLS LIST ANIMATION ====================*/
+const skillsContent = document.getElementsByClassName('skill techs'),
+      skillHeader = document.querySelectorAll('.skill-header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skill techs skills_close'
+    }
+    if(itemClass === 'skill techs skills_close'){
+        this.parentNode.className = 'skill techs skills_open'
+    }
+}
+
+skillHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
